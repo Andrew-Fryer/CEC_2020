5f518e631260fe8aeb41df3147a0ab8f1ed592c1
And1210 <amf7crazy@gmail.com>
2020-03-07 11:41:09 -0600

    Made algorithm into class

--- blocks.py
+++ blocks.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-# all of these lists will be for instances of the classes
-correct = []  # which z=0 positions are in the correct place
-# need a stack object
-emptySpaces = []  # empty space available.
-unknown = []  # list of 2d visualization with which ones are stacks we don't know below
+class Algorithm:
+    def __init__(self):
+        # all of these lists will be for instances of the classes
+        self.correct = []  # which z=0 positions are in the correct place
+        # need a stack object
+        self.emptySpaces = []  # empty space available.
+        self/unknown = []  # list of 2d visualization with which ones are stacks we don't know below
+    
