6b6c1961e8fcaaf4d8768197f36dff573a214144
ksinger12 <kylesinger@rogers.com>
2020-03-07 11:47:18 -0600

    created storage components

--- blocks.py
+++ blocks.py
@@ -1,10 +1,10 @@
 import numpy as np
 
+
 class Algorithm:
     def __init__(self):
         # all of these lists will be for instances of the classes
         self.correct = []  # which z=0 positions are in the correct place
-        # need a stack object
+        self.stacks = dict.fromkeys(['x', 'y', 'z'], 0)
         self.emptySpaces = []  # empty space available.
-        self/unknown = []  # list of 2d visualization with which ones are stacks we don't know below
-    
+        self.unknown = []  # list of 2d visualization with which ones are stacks we don't know below
