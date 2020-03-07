fdf8c8f34d4afb998cff2cfa2a95a061e590edf8
ksinger12 <kylesinger@rogers.com>
2020-03-07 13:39:56 -0600

    fixed temp file

--- temp.py
+++ temp.py
@@ -1,9 +1,21 @@
 import numpy as np
 
+stacks = []
+stacks.append({'x': 5, 'y': 5})
+stacks.append({'x': 6, 'y': 6})
+drone = {'x': 3, 'y': 0}
+
 
 def closestStack(stacks, drone):
     distance = []
-    for i in len(stacks):
-        distance[i] = abs(drone.x - stacks[i].x + drone.y - stacks[i].y)
-    stackIndex = distance.index(min(distance))
-    return stackIndex
+    orderedStack = []
+    for i in range(0, len(stacks)):
+        distance.append(abs(drone['x'] - stacks[i]['x'] +
+                            drone['y'] - stacks[i]['y']))
+    distanceSorted = sorted(distance)
+    for j in range(0, len(stacks)):
+        orderedStack.append(stacks[distance.index(distanceSorted[j])])
+    return orderedStack
+
+
+print(closestStack(stacks, drone))
