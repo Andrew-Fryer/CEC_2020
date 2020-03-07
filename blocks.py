0a9543a70a9244f2376ea929b2226700e49190ac
ksinger12 <kylesinger@rogers.com>
2020-03-07 11:38:31 -0600

    starting algorithm design

---
+++ blocks.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# all of these lists will be for instances of the classes
+correct = []  # which z=0 positions are in the correct place
+# need a stack object
+emptySpaces = []  # empty space available.
+unknown = []  # list of 2d visualization with which ones are stacks we don't know below
